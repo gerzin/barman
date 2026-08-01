a833cff1b2ec4b361d6723d469eea73b19dfc2a3
Gerardo Zinno <zinno.gerardo.95@gmail.com>
2026-08-01 15:32:07 +0200

    Rm vibecoded frontend

--- frontend/barman/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { MenuSkeleton } from "@/components/menu/menu-skeleton"
-
-export default function Loading() {
-    return (
-        <main className="mx-auto flex w-full max-w-2xl flex-1 flex-col gap-4 px-4 py-4">
-            <MenuSkeleton />
-        </main>
-    )
-}
